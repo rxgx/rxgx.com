35a11f0c339826c8cb33c6356d5c04c0c0b06f15
Ryan Gasparini <ryan@rxgx.com>
2020-11-10 03:19:51 -0800

    added figure html

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Link from 'next/link'
 import AboutPage from '../components/AboutPage'
 import Card from '../components/Card'
+import Figure from '../components/AboutPage/Figure'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
@@ -20,37 +21,9 @@ function HomePage () {
         <link rel='stylesheet' type='text/css' href='./css/base.css' />
         <link rel='stylesheet' type='text/css' href='./css/style.css' />
       </Head>
+      <Figure />
       <Header />
-      <article id='body'>
-        <div className='section cards'>
-          <Card>
-            Anyone else feel that less and sass are not addressing any of the
-            problems with #CSS?
-            <a href=''>Why we don't do apps</a>
-          </Card>
-
-          <div className='card'>
-            Have you heard about responsive design? Of course, we've read the
-            Smashing!
-            <a href=''>Need to catch up with tech?</a>
-          </div>
-
-          <div className='card'>
-            You paid for a redesign but they delivered a hacked together
-            Wordpress template.
-            <a href=''>Are you totes screwed?</a>
-          </div>
-
-          <div className='card'>
-            Not sure about mobile first. Is that a category for a Commercial
-            Arts award?
-            <a href=''>Tired of creative misdirection?</a>
-          </div>
-        </div>
-      </article>
-
       <AboutPage />
-
       <Footer />
     </div>
   )
